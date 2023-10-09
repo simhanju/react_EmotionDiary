2fbc3a4b10172a01b89d4cdb49c68f987b3361ed
simhanju <101966570+simhanju@users.noreply.github.com>
2023-10-09 21:42:25 +0900

    Feat: Home 페이지 구현
    fliter 기능에서 오류가 발생하오니 알아서 수정 할것.

--- EmotionDiary/src/App.js
+++ EmotionDiary/src/App.js
@@ -1,43 +1,134 @@
 import './App.css';
+import React, { useReducer, useRef } from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
-// components
-import MyButton from './components/MyButton';
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE':{
+      const newItem = {
+        ...action.data,
+      };
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it)=> it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT':{
+      newState = state.map((it) => 
+        it.id === action.data.id ? {...action.data} : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+}
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    data: 1696660848757,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    data: 1696660848758,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    data: 1696660848759,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    data: 1696660848760,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    data: 1696660848761,
+  },
+];
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, dummyData);
+  const dataId = useRef(0);
+  
+  //CREATE
+  const onCreate = (date, content, emotion)=>{
+    dispatch({
+      type : "CREATE", 
+      data:{
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      }
+    });
+    dataId.current += 1;
+  };
+
+  // REMOVE
+  const onRemove = (targetId) =>{
+    dispatch({type:"REMOVE", targetId});
+  };
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) =>{
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h2>App.js</h2>
-
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={""}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"negative"}
-        />
-
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/new" element={<New />}/>
-          <Route path="/edit" element={<Edit />}/>
-          <Route path="/diary/:id" element={<Diary />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider 
+        value={{
+          onCreate,
+          onEdit,
+          onRemove,
+        }}
+      >
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
